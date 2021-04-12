0f17dfdad05159d002aeda8eb05c43411b6a2925
Renju Jose <renju.jose@testsigma.com>
2021-04-12 22:59:49 +0530

    Code refactoring
    - Renamed sample to Xml2Html
    - Added file exists check
    - Added command line arguments
    - Added sample TS Junit report stylesheet and inputs

--- Xml2Html.ps1
+++ Xml2Html.ps1
@@ -1,25 +1,49 @@
-$source = @"
-using System;
-using System.IO;
-using System.Xml;
-using System.Xml.Xsl;
+using namespace System;
+using namespace System.IO;
+using namespace System.Xml;
+using namespace System.Xml.Xsl;
 
-public class Xml2Html
-{
-  public static string Transform(string inputXml, string xslt)
-  {
-    var transform = new XslCompiledTransform();
-    using(var reader = XmlReader.Create(new StringReader(xslt)))
-    {
-      transform.Load(reader);
+if ($args.count -lt 3) {
+    Write-Error -Message "Required input files not found." -Category InvalidArgument
+    Write-Error "For Example: ./sample.ps1 ./sample.xml ./sample.xslt ./sample.html"
+    if (!$args[0]) {
+        Write-Error "Please pass input XML file path as first input!";
     }
-    var results = new StringWriter();
-    using(var reader = XmlReader.Create(new StringReader(inputXml)))
-    {
-        transform.Transform(reader, null, results);
+    if (!$args[1]) {
+        Write-Error "Please pass XSLT file path as second input";
     }
-    return results.ToString();
-  }
+    if (!$args[2]) {
+        Write-Error "Please pass output HTML file path as third input";
+    }
+    Exit
 }
-"@
-Add-Type -TypeDefinition $source -PassThru | Out-Null
+elseif ($args.count -eq 3) {
+    $xmlFile=$args[0];
+    $xsltFile=$args[1];
+    $htmlFile=$args[2];
+    #(Test-Path $args[0] -PathType leaf) && (Test-Path $args[1] -PathType leaf) && (Test-Path $args[2] -PathType leaf)
+    if((Test-Path $args[0] -PathType leaf) -And (Test-Path $args[1] -PathType leaf)){
+        Write-Host "All input files loaded successfully. Output HTML file will be created at the end.";
+    }
+    else{
+        Write-Error "Please check that all the input files exist at the given path!"
+        Exit
+    }
+}
+
+$transform = New-Object System.Xml.Xsl.XslCompiledTransform
+
+Write-Host "Loading '$xsltFile' stylesheet..."
+$xsltreader = [System.Xml.XmlReader]::Create($xsltFile);
+try{
+    $transform.Load($xsltreader)
+}
+catch{
+    Write-Error "Failed to compile Stylesheet. Please check the contents."
+    Exit
+}
+
+Write-Host "Transforming '$xmlFile' to HTML Format..."
+$transform.Transform($xmlFile,$htmlFile);
+Write-Host "Html saved to '$htmlFile'" -ForegroundColor Green
+Write-Host -ForegroundColor White
